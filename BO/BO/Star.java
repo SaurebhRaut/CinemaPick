835eab344c831e4cee0baf272448a5c3e654e59f
TorexIDE <rauts@uci.edu>
2015-06-07 20:03:08 -0700

    Third commit- connection pooling, data caching, session management (shopping cart), dyanmic pagging

--- BO/BO/Star.java
+++
@@ -1,9 +0,0 @@
-package BO;
-
-public class Star {
-	public int id; //int primary key AUTO_INCREMENT,
-	public String first_name; //varchar(50) not null,
-	public String last_name; // varchar(50) not null,
-	public String dob; //date ,
-	public String photo_url; // varchar(200)
-}
